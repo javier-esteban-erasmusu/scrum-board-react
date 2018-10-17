311ba87e12fe75682d955358c18d0a3ef04c00e4
Javier Esteban <javier.esteban@erasmusu.com>
2018-10-17 18:41:22 +0200

    Añadidos ficheros iniciales de Redux

---
+++ src/store/reducer.js
@@ -0,0 +1,10 @@
+import initialState from 'initialState';
+
+export default reducer(state = initialState, action)
+{
+    switch(action)
+    {
+        default:
+            return state;
+    }
+}
