750df1517677af4fa7d18882de60f34d8dce9cd1
Javier Esteban <javier.esteban@erasmusu.com>
2018-10-17 19:38:39 +0200

    Añadido react-redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store/index.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
